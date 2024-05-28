401d6a404e3d8f5d15a45ddb1e0ee767b774866b
Josef Shainskiy <josef2011@gmail.com>
2024-05-28 22:59:46 +0300

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavComponent} from "./components/nav/nav.component";
+import {ThemeService} from "./services/theme.service";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tennis';
+  themeService: ThemeService = inject(ThemeService);
 }
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import {ThemeService} from "../../services/theme.service";
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    AsyncPipe,
+    RouterOutlet,
+  ]
+})
+export class NavComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+  themeService: ThemeService = inject(ThemeService);
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  toggleTheme() {
+    this.themeService.updateTheme();
+  }
+}
